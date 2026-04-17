d2c0b8a97b76ef993e557ecfff47250a2ef3b5cb
OEvortex <abhat8283@gmail.com>
2026-04-17 17:46:12 +0530

    Delete all files except OLD_ARCHITECTURE.md
    Keeping only the architecture documentation as reference for future development.
    All code, configuration, and other files have been removed.

--- jprinter/builtins.py
+++
@@ -1,9 +0,0 @@
-from .jprint import jprint
-try:
-    builtins = __import__('__builtin__')
-except ImportError:
-    builtins = __import__('builtins')
-def install(ic='jprint'):
-    setattr(builtins, ic, jprint)
-def uninstall(ic='jprint'):
-    delattr(builtins, ic)
